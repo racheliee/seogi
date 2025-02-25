7be0bac5f6d8116df57596c32d36b0c713fa98f2
racheliee <rachelie.dwg@gmail.com>
2025-02-25 15:16:30 +0900

    init mac app

---
+++ mac-app/mac-app/mac_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_appApp.swift
+//  mac-app
+//
+//  Created by rachelie on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct mac_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mac-app/mac-appTests/mac_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  mac_appTests.swift
+//  mac-appTests
+//
+//  Created by rachelie on 2/25/25.
+//
+
+import Testing
+@testable import mac_app
+
+struct mac_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
